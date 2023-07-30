4f523b06f10fc4b62eb58cfe246966a3640d1af2
Rohit Gusain <57216224+i-am-rohit@users.noreply.github.com>
2023-07-31 04:47:16 +0530

    Add files via upload

--- decorator.py
+++ decorator.py
@@ -1,18 +1,27 @@
+# @ use for decorator
+
+
 def dec_func(func):
     def wrap():
         print("This is Awesom Function")
     func()
     return  wrap
+
+@dec_func
 def func1():
     print("This is function 1")
 
+func1()
+
+
+@dec_func
 def func2():
     print("This is function 2")
 #
-# func1()
-# func2()
 
-var1 = dec_func(func1)
-var1()
-var2 = dec_func(func2)
-var2()
+func2()
+#
+# var1 = dec_func(func1)
+# var1()
+# var2 = dec_func(func2)
+# var2()
